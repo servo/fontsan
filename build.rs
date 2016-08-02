d2eb58532db429ada86fb3065d05f075ff446742
Khaled Hosny <khaledhosny@eglug.org>
2016-08-02 14:46:33 +0200

    Build ots_glue with CMake as well

--- build.rs
+++ build.rs
@@ -1,26 +1,19 @@
 #![deny(warnings)]
 
 extern crate cmake;
-extern crate gcc;
 
 use std::env;
 
 fn main() {
     let dst = cmake::Config::new("src").build();
 
-    gcc::Config::new()
-        .cpp(true)
-        .file("src/ots_glue.cc")
-        .flag("-O3").flag("-fPIC")
-        .include("src/ots/include/")
-        .compile("libots_glue.a");
-
     println!("cargo:rustc-link-search=native={}\n\
               cargo:rustc-link-search=native={}/lib\n\
               cargo:rustc-link-lib=static=miniz\n\
               cargo:rustc-link-lib=static=brotli\n\
               cargo:rustc-link-lib=static=woff2\n\
-              cargo:rustc-link-lib=static=ots",
+              cargo:rustc-link-lib=static=ots\n\
+              cargo:rustc-link-lib=stdc++",
         env::var("DEP_MINIZ_ROOT").unwrap(),
         dst.display());
 }
