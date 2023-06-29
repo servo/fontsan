5f75323899978465e099048fed97be57c0d7b397
Martin Robinson <mrobinson@igalia.com>
2023-06-29 11:46:15 +0200

    Better path joining

--- build.rs
+++ build.rs
@@ -3,15 +3,20 @@
 extern crate cmake;
 
 use std::env;
+use std::path::Path;
 
 fn main() {
     let dst = cmake::Config::new("src").build();
+    let lib_dest_path = Path::new(&dst)
+        .join("lib")
+        .into_os_string()
+        .into_string()
+        .expect("Could not turn path into string.");
 
-    println!("cargo:rustc-link-search=native={}/lib\n\
+    println!("cargo:rustc-link-search=native={lib_dest_path}\n\
               cargo:rustc-link-lib=static=brotli\n\
               cargo:rustc-link-lib=static=woff2\n\
-              cargo:rustc-link-lib=static=ots",
-        dst.display());
+              cargo:rustc-link-lib=static=ots");
     let target = env::var("TARGET").unwrap();
     if target.contains("apple") {
         println!("cargo:rustc-link-lib=c++");
