1f144b45f9fe9e5e8f36a7e16feb521754541423
Jonathan Schwender <schwenderjonathan@gmail.com>
2024-03-20 20:44:32 +0100

    Replace cmake with cc-rs
    Use cc-rs for the build of the native libraries.
    This removes the dependency on `cmake`, which hopefully solves the
    following issues:
    
    - `cc-rs` will link to the correct C++ stdlib depending on the compiler
       and the `CXXSTDLIB` / `CXXSTDLIB_$target` variables. Our buildscript
       previously only had a very simple target-based approach.
    - Fix random cases when cross-compiling (e.g. to -ohos), where the
      `CMakeCache.txt`would contain the default host compiler and not
      respect the CMake toolchain file.
      This only happened when recompiling, and might involve a race
      depending on when exactly the parent servo compilation failed.

--- build.rs
+++ build.rs
@@ -1,21 +1,80 @@
 #![deny(warnings)]
 
-extern crate cmake;
+use std::path::{Path, PathBuf};
 
-use std::env;
+const BROTLI_INCLUDE_DIR: &str = "src/deps/brotli/c/include";
+const LZ4_INCLUDE_DIR: &str = "src/deps/lz4/lib";
+const OTS_INCLUDE_DIR: &str = "src/deps/ots/include";
+const WOFF2_INCLUDE_DIR: &str = "src/deps/woff2/include";
+const ZLIB_INCLUDE_DIR: &str = "src/fake-zlib";
+
+fn build_lz4() {
+    cc::Build::new()
+        .file("src/deps/lz4/lib/lz4.c")
+        .compile("lz4");
+}
+
+fn build_ots() {
+    let ots_sources = glob::glob("src/deps/ots/src/*.cc")
+        .expect("Invalid glob pattern")
+        .collect::<Result<Vec<PathBuf>, _>>()
+        .expect("vendored ots sources not found");
+
+    cc::Build::new()
+        .cpp(true)
+        .files(ots_sources)
+        .include(OTS_INCLUDE_DIR)
+        .include(LZ4_INCLUDE_DIR)
+        .include(WOFF2_INCLUDE_DIR)
+        .include(ZLIB_INCLUDE_DIR)
+        .std("c++11")
+        .compile("ots");
+}
+
+fn build_brotli() {
+    let brotli_sources = glob::glob("src/deps/brotli/c/**/*.c")
+        .expect("Invalid glob pattern")
+        .collect::<Result<Vec<PathBuf>, _>>()
+        .expect("vendored brotli sources not found");
+
+    cc::Build::new()
+        .files(brotli_sources)
+        .include(BROTLI_INCLUDE_DIR)
+        .compile("brotli");
+}
+
+fn build_woff2() {
+    let woff2_dir = Path::new("src/deps/woff2/src");
+    let file_names = [
+        "table_tags.cc",
+        "variable_length.cc",
+        "woff2_common.cc",
+        "woff2_dec.cc",
+        "woff2_out.cc",
+    ];
+    let woff2_sources = file_names.iter().map(|name| woff2_dir.join(name));
+
+    cc::Build::new()
+        .files(woff2_sources)
+        .include(WOFF2_INCLUDE_DIR)
+        .include(BROTLI_INCLUDE_DIR)
+        .std("c++11")
+        .warnings(false)
+        .compile("woff2");
+}
+
+fn build_ots_glue() {
+    cc::Build::new()
+        .file("src/ots_glue.cc")
+        .include(OTS_INCLUDE_DIR)
+        .std("c++11")
+        .compile("ots_glue");
+}
 
 fn main() {
-    let dst = cmake::Config::new("src").build();
-
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=static=ots");
-    println!("cargo:rustc-link-lib=static=woff2");
-    println!("cargo:rustc-link-lib=static=brotli");
-    let target = env::var("TARGET").unwrap();
-
-    if target.contains("apple") || target.contains("ohos") {
-        println!("cargo:rustc-link-lib=c++");
-    } else if !target.contains("msvc") {
-        println!("cargo:rustc-link-lib=stdc++");
-    }
+    build_lz4();
+    build_ots();
+    build_brotli();
+    build_woff2();
+    build_ots_glue();
 }
